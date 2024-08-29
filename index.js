27c7ad111cb339b752a7a8151b8e882d78b92136
dp00718 <156597537+dp00718@users.noreply.github.com>
2024-08-29 19:08:24 +0530

    index

--- index.js
+++ index.js
@@ -371,7 +371,7 @@ app.use(express.static(path.join(__dirname, 'Assets')));
           return res.status(401).json({ error: 'Invalid token' });
         }
   
-        const user = await User1.findOne({ emailaddress: decoded.email });
+        const user = await User1.findOne({ emailaddress });
         if (!user) {
           return res.status(404).json({ error: 'User not found' });
         }
